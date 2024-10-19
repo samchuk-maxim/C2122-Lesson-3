e4b463f3b00f0ce8c9b08c4b39c38fad05dd4339
samchuk-maxim <mk2b79@gmail.com>
2024-10-19 16:19:03 +0300

    Add class Student to main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print("Lesson 3")
+
+class Student:
+    def __init__(self,Name,Surname,Age):
+        self.Name=Name
+        self.Surname=Surname
+        self.Age=Age
+        print("Create Seccesfull")
+
+    def print_student(self):
+        print("Surname:",self.Surname,"\tName:",self.Name,"\tAge:",self.Age)
+
+
+Student1 = Student("Vitalik","Oxygen",42)
+Student2 = Student("Boris","Britva",24)
+
+Student1.print_student()
+Student2.print_student()
+
